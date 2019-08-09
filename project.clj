529ab3463def9d596494d9562dd2e6c9e66d2ebe
James Carnegie <kipz@users.noreply.github.com>
2019-08-09 09:55:35 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [clj-time "0.15.9"]
                  [org.clojure/tools.logging "0.4.2"]
                  [clj-http-lite "0.3.0"]])
