45d6d0332a202055bb3a3761f2424f08e456c156
James Carnegie <kipz@users.noreply.github.com>
2019-08-12 17:33:45 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.2"]
                  [clj-time "0.15.9"]
                  [kipz-test "1.2.3"]
-                 [api-demo "0.9.0"]
+                 [api-demo "blah-blah"]
                  [org.clojure/tools.logging "0.4.2"]
                  [clj-http-lite "0.3.1"]])
